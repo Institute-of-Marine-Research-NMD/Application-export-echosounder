3d1f45b7eb6899a94d357fe0eb0dddb3bab6a52b
= <sjur.ringheim.lid@imr.no>
2016-02-10 12:49:29 +0100

    Changed the cron timer so that it should now run when scheduled

--- src/main/java/no/imr/nmdapi/client/loader/config/CamelConfig.java
+++ src/main/java/no/imr/nmdapi/client/loader/config/CamelConfig.java
@@ -3,8 +3,8 @@ package no.imr.nmdapi.client.loader.config;
 import no.imr.messaging.processor.ExceptionProcessor;
 import no.imr.nmdapi.exceptions.CantWriteFileException;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.routepolicy.quartz.CronScheduledRoutePolicy;
 import org.apache.camel.spring.javaconfig.SingleRouteCamelConfiguration;
+import org.apache.camel.util.UnsafeUriCharactersEncoder;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -28,11 +28,9 @@ public class CamelConfig extends SingleRouteCamelConfiguration implements Initia
 
             @Override
             public void configure() {
-                CronScheduledRoutePolicy startPolicy = new CronScheduledRoutePolicy();
-                startPolicy.setRouteStartTime(config.getString("cron.activation.time"));
 
                 onException(CantWriteFileException.class).continued(true).process(new ExceptionProcessor(config.getString("application.name"))).to("jms:queue:".concat(config.getString("queue.outgoing.error")));
-                from("timer://runOnce?repeatCount=1&delay=5000").routePolicy(startPolicy)
+                from("quartz://cacheRefresh?cron="+UnsafeUriCharactersEncoder.encode(config.getString("cron.activation.time")))
                         .to("getAllEchosounderDatasets")
                         .split(body())
                         .to("echosounderLoaderService")
